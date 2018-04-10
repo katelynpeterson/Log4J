f00c1e9258d01d1d21ab177b3d1c537d8bc2d9bf
katelynpeterson <33292506+katelynpeterson@users.noreply.github.com>
2018-04-10 11:41:52 -0600

    add files

---
+++ Log4j/src/test/Log4JPropertiesTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.apache.log4j.Logger;
+
+public class Log4JPropertiesTest {
+	static java.util.logging.Logger log = Logger.getLogger(Log4JPropertiesTest.class);
+	
+	public static void main(String [] args) {
+		log.debug("This is a debug message");
+		myMethod();
+		log.info("This is an info message");
+	}
+	
+	private static void myMethod() {
+		try {
+			throw new Exception("My Exception");
+		}catch(Exception e) {
+			log.error("This is an exception", e);
+		}
+	}
+}
